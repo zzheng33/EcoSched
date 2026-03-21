d7f0b2527e0e4871e27928b6b50ec98b1f7945e4
Zhong Zheng <ac.zzheng@hopper00.jlse.itd.alcf.anl.gov>
2026-03-21 20:08:05 +0000

    /

--- GPGPU/coSched/run.sh
+++ GPGPU/coSched/run.sh
@@ -8,22 +8,22 @@ PYTHON="$HOME/venv_sched/bin/python"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR"
 
-$PYTHON run_cosched_sequential.py --policy sequential --sequential-gpu-strategy best
+# $PYTHON run_cosched_sequential.py --policy sequential --sequential-gpu-strategy best
 
 
-$PYTHON run_cosched_sequential.py --policy sequential --sequential-gpu-strategy max
+# $PYTHON run_cosched_sequential.py --policy sequential --sequential-gpu-strategy max
 
 
-$PYTHON solve_energy_optimal_cpsat.py --time-limit 300
+$PYTHON solve_energy_optimal_cpsat.py --time-limit 1200
 
 
 $PYTHON run_solver_schedule.py
 
 
-$PYTHON run_cosched_marble.py
+# $PYTHON run_cosched_marble.py
 
 
-$PYTHON ecoPack.py --policy cmab
+# $PYTHON ecoPack.py --policy cmab
 
 
 # $PYTHON ecoPack.py --policy heuristic
